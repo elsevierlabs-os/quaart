f0111590dc01aefc066472a084767554cbe7e26f
Corey Harper <c.harper@elsevier.com>
2022-07-29 14:27:12 -0400

    more data fixes

--- data-prep-script.sh
+++ data-prep-script.sh
@@ -9,14 +9,14 @@ then
     wget https://raw.githubusercontent.com/xiaoling/figer/master/aaai/exp.label -P data/output/
     wget https://raw.githubusercontent.com/xiaoling/figer/master/aaai/exp.txt -P data/output/
     echo "Downloading HAnDS data:"
-    wget https://raw.githubusercontent.com/abhipec/HAnDS/master/datasets/1k-WFB-g/1k-WFB-g_complete.json -P data/output
+    wget https://raw.githubusercontent.com/abhipec/HAnDS/master/datasets/1k-WFB-g/1k-WFB-g_complete.json -P data/output/
     wget https://raw.githubusercontent.com/abhipec/HAnDS/master/datasets/1k-WFB-g/fner_dev.json -P data/output/
     wget https://raw.githubusercontent.com/abhipec/HAnDS/master/datasets/1k-WFB-g/fner_test.json -P data/output/
     cat data/output/fner_dev.json | jq -cs '.' > data/output/fner_dev_array.json
     cat data/output/fner_test.json | jq -cs '.' > data/output/fner_test_array.json
     cat data/output/1k-WFB-g_complete.json | jq -cs '.' > data/output/1k-WFB-g_complete_array.json
     python hands_gdown.py
-    unzip data/output/HAnDS_figer_types_stage_one_state_two_sentences_stage_three_pp.tar.gz
+    tar -xvfz data/output/HAnDS_figer_types_stage_one_state_two_sentences_stage_three_pp.tar.gz -C data/output/
     echo "Download complete"
 else
     echo "data/output exists - delete to redownolad"
